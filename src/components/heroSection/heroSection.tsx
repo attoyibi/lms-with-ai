4f6fe2cb8eb70db6a927dd7d2d6ddd44f33f8374
attoyibi <52569071+muhammad-muchson@users.noreply.github.com>
2023-10-08 13:43:22 +0700

    slicing navbar

---
+++ src/components/heroSection/heroSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-row content-center">
+      <div className="basis-1/2 place-items-center">01222</div>
+      <div className="basis-1/2 place-items-center">02</div>
+    </div>
+  );
+};
+
+export default HeroSection;
