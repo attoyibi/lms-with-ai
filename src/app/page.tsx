7265838233871f76d9fc123e1b8d3f2fa625adb3
attoyibi <muchson@alterra.id>
2024-05-09 16:00:27 +0700

    assesment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,85 @@
-
-import HeroSection from "@/components/heroSection/heroSection";
-import CourseList from "@/components/courseList/courseList";
-import VideoList from "@/components/videoList/videoList";
-import Navbar from "@/components/navbar/navbar";
-import { createClient } from '@supabase/supabase-js'
+import HeroSection from '@/components/heroSection/heroSection';
+import CourseList from '@/components/courseList/courseList';
+import VideoList from '@/components/videoList/videoList';
+import Navbar from '@/components/navbar/navbar';
+import { createClient } from '@supabase/supabase-js';
 import supabase from '@/lib/supabase';
 export const courses = [
-  {
-    id: 1,
-    title: 'Introduction to Web Development',
-    description: 'Learn the basics of web development using HTML, CSS, and JavaScript.',
-    level: 'Beginner',
-    duration: '4 weeks',
-    imageUrl: '', // Assuming image is in the 'images' folder
-  },
-  {
-    id: 2,
-    title: 'React.js Fundamentals',
-    description: 'Master the fundamentals of React.js and build interactive web applications.',
-    level: 'Intermediate',
-    duration: '6 weeks',
-  },
-  {
-    id: 3,
-    title: 'Node.js and Express.js for Backend Development',
-    description: 'Explore backend development with Node.js and Express.js.',
-    level: 'Intermediate',
-    duration: '8 weeks',
-  },
-  {
-    id: 4,
-    title: 'Advanced JavaScript Concepts',
-    description: 'Dive deep into advanced JavaScript concepts such as closures, promises, and async/await.',
-    level: 'Advanced',
-    duration: '5 weeks',
-  },
-  {
-    id: 5,
-    title: '6',
-    description: 'Dive deep into advanced JavaScript concepts such as closures, promises, and async/await.',
-    level: 'Advanced',
-    duration: '6 weeks',
-  },
+	{
+		id: 1,
+		title: 'Introduction to Web Development',
+		description: 'Learn the basics of web development using HTML, CSS, and JavaScript.',
+		level: 'Beginner',
+		duration: '4 weeks',
+		imageUrl: '' // Assuming image is in the 'images' folder
+	},
+	{
+		id: 2,
+		title: 'React.js Fundamentals',
+		description: 'Master the fundamentals of React.js and build interactive web applications.',
+		level: 'Intermediate',
+		duration: '6 weeks'
+	},
+	{
+		id: 3,
+		title: 'Node.js and Express.js for Backend Development',
+		description: 'Explore backend development with Node.js and Express.js.',
+		level: 'Intermediate',
+		duration: '8 weeks'
+	},
+	{
+		id: 4,
+		title: 'Advanced JavaScript Concepts',
+		description: 'Dive deep into advanced JavaScript concepts such as closures, promises, and async/await.',
+		level: 'Advanced',
+		duration: '5 weeks'
+	},
+	{
+		id: 5,
+		title: '6',
+		description: 'Dive deep into advanced JavaScript concepts such as closures, promises, and async/await.',
+		level: 'Advanced',
+		duration: '6 weeks'
+	}
 ];
 
 export default function Home() {
-  return (
-    <div className="sm:overflow-x-hidden">
-      <main className="shadow-md">
-        <div className="lg:px-7 max-w-screen-xl container mx-auto ">
-          <Navbar />
-          {/* Konten lainnya */}
-        </div>
-      </main>
-      <div className="px-7 md:px-7 lg:px-7 container mx-auto max-w-screen-xl sm:overflow-x-hidden">
-        <HeroSection />
-        <CourseList courses={courses}/>
-        
-    
-        {/* <VideoList /> */}
-      </div>
-      <div className="mt-8 text-center bg-gray-100 py-6 ">
-      <p className="text-gray-800 text-lg">Explore endless possibilities and connect with me on:</p>
-      <div className="flex justify-center mt-4 space-x-4">
-        <a href="https://www.linkedin.com/in/muhammad-muchson-7024911b8/" target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">LinkedIn</a>
-        <a href="https://www.youtube.com/channel/UCXh8wC8rrctCESTKed2aDMQ" target="_blank" rel="noopener noreferrer" className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">YouTube</a>
-      </div>
-    </div>
-      <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto px-4">
-        <p className="text-center">&copy; 2024 Your Website. All rights reserved.</p>
-      </div>
-    </footer>
-    </div>
-  );
+	return (
+		<div className="sm:overflow-x-hidden">
+			<Navbar />
+			{/* Konten lainnya */}
+			<div className="px-7 md:px-7 lg:px-7 container mx-auto max-w-screen-xl sm:overflow-x-hidden">
+				<HeroSection />
+				<CourseList courses={courses} />
+
+				{/* <VideoList /> */}
+			</div>
+			<div className="mt-8 text-center bg-gray-100 py-6 ">
+				<p className="text-gray-800 text-lg">Explore endless possibilities and connect with me on:</p>
+				<div className="flex justify-center mt-4 space-x-4">
+					<a
+						href="https://www.linkedin.com/in/muhammad-muchson-7024911b8/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+					>
+						LinkedIn
+					</a>
+					<a
+						href="https://www.youtube.com/channel/UCXh8wC8rrctCESTKed2aDMQ"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+					>
+						YouTube
+					</a>
+				</div>
+			</div>
+			<footer className="bg-gray-800 text-white py-4">
+				<div className="container mx-auto px-4">
+					<p className="text-center">&copy; 2024 Your Website. All rights reserved.</p>
+				</div>
+			</footer>
+		</div>
+	);
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,72 +1,70 @@
-'use client'
-import "tailwindcss/tailwind.css"; // Import Tailwind CSS
-import Image from "next/image";
-import styles from "./navbar.module.css"; //
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+'use client';
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
+import Image from 'next/image';
+import styles from './navbar.module.css'; //
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 export default function Navbar() {
-  const router = useRouter()
+	const router = useRouter();
 
-  return (
+	return (
+		<main className="shadow-md">
+			<div className="lg:px-7 max-w-screen-xl container mx-auto ">
+				<div className="navbar bg-base-100 ">
+					<div className="navbar-start">
+						<div className="dropdown">
+							<label tabIndex={0} className="btn btn-ghost lg:hidden">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M4 6h16M4 12h8m-8 6h16"
+									/>
+								</svg>
+							</label>
+							<ul
+								tabIndex={0}
+								className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+							>
+								<li>
+									<a>Item 111</a>
+								</li>
+								<li>
+									<a>Parent</a>
+									<ul className="p-2">
+										<li>
+											<a>Submenu 11</a>
+										</li>
+										<li>
+											<a>Submenu 2</a>
+										</li>
+									</ul>
+								</li>
+								<li>
+									<a>Item 3</a>
+								</li>
+							</ul>
+						</div>
+						<div className="flex items-center">
+							<Image
+								src="/assets/images/robot.png" // Make sure the path is correct
+								alt="Logo"
+								width={25}
+								height={25}
+								className={`mr-2 w-6 h-6 sm:w-12 sm:h-12 ${styles['hidden-on-small-screen']}`} // Tambahkan kelas CSS
+							/>
 
-    
-    <div className="navbar bg-base-100 ">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 111</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 11</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex items-center">
-          <Image
-            src="/assets/images/robot.png" // Make sure the path is correct
-            alt="Logo"
-            width={25}
-            height={25}
-            className={`mr-2 w-6 h-6 sm:w-12 sm:h-12 ${styles["hidden-on-small-screen"]}`} // Tambahkan kelas CSS
-          />
-
-          <p className="items-center normal-case text-xl hidden sm:block">
-            LMS Sinau
-          </p>
-        </div>
-      </div>
-      {/* <div className="navbar-center hidden lg:flex">
+							<p className="items-center normal-case text-xl hidden sm:block">LMS Sinau</p>
+						</div>
+					</div>
+					{/* <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
             <a>Item 1</a>
@@ -89,12 +87,19 @@ export default function Navbar() {
           </li>
         </ul>
       </div> */}
-      <div className="navbar-end">
-        <a onClick={() => router.push('/assessment')} className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md me-2">
-          Try Assessment
-        </a>
-        <a onClick={() => router.push('/login')} className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md">Login</a>
-      </div>
-    </div>
-  );
+					<div className="navbar-end">
+						<a
+							onClick={() => router.push('/assessment')}
+							className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md me-2"
+						>
+							Try Assessment
+						</a>
+						<a onClick={() => router.push('/login')} className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md">
+							Login
+						</a>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
