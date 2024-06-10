0830ce926cf4b81268a6ce962e24e0e56b6415ea
attoyibi <muchson@alterra.id>
2024-06-10 22:37:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // webpack: (config, { isServer }) => {
-    //     config.resolve.alias['@'] = path.join(__dirname, 'src');
-    //     return config;
-    // },
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['@'] = path.join(__dirname, 'src');
+        return config;
+    },
 };
 
 module.exports = nextConfig;
