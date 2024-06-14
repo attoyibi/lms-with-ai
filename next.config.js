e1f6a450eae46897ab8455a26d658b14f0df68a4
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2024-06-14 21:01:06 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const path = require("path"); // Add this line at the top
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.resolve.alias["@"] = path.join(__dirname, "src");
-    return config;
-  },
-};
+const nextConfig = {};
+
+// Your code using `path` on line 4 (if applicable)
+
 module.exports = nextConfig;
