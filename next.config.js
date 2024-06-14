e4a0a929ecd1d50384c21f2323d5dfaa8173f955
alterra-muchson <52569071+muhammad-muchson@users.noreply.github.com>
2024-06-14 21:00:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const path = require('path');
+const path = require("path"); // Add this line at the top
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        config.resolve.alias['@'] = path.join(__dirname, 'src');
-        return config;
-    },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
-
 module.exports = nextConfig;
